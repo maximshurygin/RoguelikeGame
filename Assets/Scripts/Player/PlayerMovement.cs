32979d1984e2579c0c44e45d2c48f11546ec28df
maximshurygin <maximys2142@mail.ru>
2025-09-05 17:43:44 +0400

    Added animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,18 +5,19 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed;
+        [SerializeField] private Animator _animator;
         private Vector3 _movement;
         public Vector3 Movement => _movement;
+        
+        private void Update() => Move();
         
         private void Move()
         {
             _movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
             transform.position += _movement.normalized * (_moveSpeed * Time.deltaTime);
-        }
-        
-        private void Update()
-        {
-            Move();
+            _animator.SetFloat("Horizontal", _movement.x);
+            _animator.SetFloat("Vertical", _movement.y);
+            _animator.SetFloat("Speed", _movement.sqrMagnitude);
         }
     }
 }
