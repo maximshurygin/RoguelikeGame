9be4dbaf4b0575d41d9cb546b2e7f9b696ca170b
maximshurygin <maximys2142@mail.ru>
2025-09-09 11:36:44 +0400

    Refactor EnemyMove and PlayerMovement

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -13,19 +13,14 @@ namespace Enemy
         private Vector3 _direction;
         private PlayerMovement _playerMovement;
         private WaitForSeconds _checkTime = new WaitForSeconds(3f);
-        private Coroutine _distanceToHide;
         
         [Inject] private void Construct(PlayerMovement playerMovement) => _playerMovement = playerMovement;
 
         private void OnEnable()
         {
-            _distanceToHide = StartCoroutine(CheckDistanceToHide());
+            StartCoroutine(CheckDistanceToHide());
         }
-
-        private void OnDisable()
-        {
-            StopCoroutine(_distanceToHide);
-        }
+        
         
         private void Update() => Move();
         private void Move()
@@ -38,7 +33,7 @@ namespace Enemy
 
         private IEnumerator CheckDistanceToHide()
         {
-            while (true)
+            while (enabled)
             {
                 float distance = Vector3.Distance(transform.position, _playerMovement.transform.position);
                 if (distance > 20f)
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,24 +2,30 @@ using UnityEngine;
 
 namespace Player
 {
+    // enum для выбора способа управления персонажем
+    enum Device
+    {
+        Keyboard,
+        Joystick
+    }
+    
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed;
         [SerializeField] private Animator _animator;
         [SerializeField] private Joystick _joystick;
-        [SerializeField] private bool _useJoystick;
+        private Device _device;
         private bool _isInteracting;
         private Vector3 _movement;
-        public Vector3 Movement => _movement;
 
         private void Update()
         {
             // если игрок двигает джойстик, то используем джойстик
             if (_joystick && (_joystick.Horizontal != 0f || _joystick.Vertical > 0f))
-                _useJoystick = true;
+                _device = Device.Joystick;
             // если игрок нажимает клавиши, то используем клавиатуру
             else if (Input.GetAxisRaw("Horizontal") != 0f || Input.GetAxisRaw("Vertical") != 0f)
-                _useJoystick = false;
+                _device = Device.Keyboard;
             
             Move();
             Interact();
@@ -27,8 +33,8 @@ namespace Player
         
         private void Move()
         {
-            float horizontal = _useJoystick && _joystick ? _joystick.Horizontal : Input.GetAxisRaw("Horizontal");
-            float vertical = _useJoystick && _joystick ? _joystick.Vertical : Input.GetAxisRaw("Vertical");
+            float horizontal = _device == Device.Joystick&& _joystick ? _joystick.Horizontal : Input.GetAxisRaw("Horizontal");
+            float vertical = _device == Device.Joystick&& _joystick ? _joystick.Vertical : Input.GetAxisRaw("Vertical");
             
             _movement = new Vector3(horizontal, vertical, 0);
             transform.position += _movement.normalized * (_moveSpeed * Time.deltaTime);
